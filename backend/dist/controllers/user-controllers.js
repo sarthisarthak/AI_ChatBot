6a16e81a0a93a5a0f3a3383f17c98fe459da8256
sarthisarthak <140862504+sarthisarthak@users.noreply.github.com>
2024-11-20 01:01:54 +0530

    Token_Verification_Done

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -87,4 +87,22 @@ export const userLogin = async (req, res, next) => {
         return res.status(404).json({ message: "ERROR", cause: error.message });
     }
 };
+export const verifyUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(res.locals.jwtData.id);
+        if (!user) {
+            return res.status(401).send("User not registered or Token malfunctioned");
+        }
+        if (user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).send("Permissions didn't match");
+        }
+        return res
+            .status(200)
+            .json({ message: "OK", name: user.name, email: user.email });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "ERROR", cause: error.message });
+    }
+};
 //# sourceMappingURL=user-controllers.js.map
